6593dc5b298aa3cf0b755b059ebc5e01f75af035
Superbia101 <cherkmark@yandex.ru>
2024-09-13 10:20:15 +0800

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,12 +1,12 @@
-"""факториал числа"""
-def fact(n):
-    a = 1
-    if n > 0:
-        for i in range(1, n + 1):
-            a = a * i
-        print(a)
-    else:
-        print("Отрицательное - не подходит!")
-        
-temp=int(input("Ведите число: "))
+def fact(num):
+    "Факториал числа"
+  comp = 1
+  if num > 0:
+      for i in range(1, num + 1):
+          comp *= i
+      print('Факторил числа', num, 'равен', comp)
+  else:
+      print("Отрицательное - не подходит!")
+
+temp = int(input("Введите число: "))
 fact(temp)
