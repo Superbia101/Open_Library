51e6f2c3df694af54d1aa3fc6aa63c348268b291
Superbia101 <cherkmark@yandex.ru>
2024-09-16 10:39:01 +0800

    Update line_palindrome.py

--- line_palindrome.py
+++ line_palindrome.py
@@ -1,15 +1,11 @@
 def palindrome(lines):
     "Проверяет строку является ли она палиндромом"
     text, temp = '', 0
-    for j in lines.lower().split():
+    for j in lines.lower().split(): # убираем лишнее
         text += j.strip(".,:;-!?")
-    for i in range(len(text)):
+    for i in range(len(text) // 2): # подсчёт одинаковых значений с начала и конца
         temp += 1 if text[i] == text[len(text) - 1 - i] else 0
-        if temp == len(text) // 2:
-            print('Палиндром!')
-            break
-    else:
-        print('Не палиндром!')
+    print('Да, это палиндром!') if temp == len(text) // 2 else print('Нет, это не палиндром!')
 
 #t = input('Введите строку: ')
 #t = 'казак'
