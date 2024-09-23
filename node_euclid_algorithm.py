c612b00a0de239dbd25c4412f6a0d32aef587456
Superbia101 <cherkmark@yandex.ru>
2024-09-23 19:50:22 +0800

    23.09.24

---
+++ node_euclid_algorithm.py
@@ -0,0 +1,10 @@
+def node_euclid_algorithm(num_1, num_2):
+    """поиск наибольшего общего делителя, через алгоритм Евклида"""
+  num_1, num_2 = max(num_1, num_2), min(num_1, num_2)
+  while num_1 % num_2 != 0:
+    num_1, num_2 = num_2, num_1 % num_2
+  print('НОД двух чисел:', num_2)
+
+max_common_div(int(input('Введите 1-е число: ')),
+               int(input('Введите 2-е число: ')))
+#print('НОД двух чисел:', gcd(num_1, num_2)) # проверка результата
