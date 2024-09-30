b24391ed3efa3b1cdc3edbde8f4bdb5e4fc43860
Superbia101 <cherkmark@yandex.ru>
2024-09-30 13:54:44 +0800

    30.09.24

--- bubble_sorting.py
+++ bubble_sorting.py
@@ -1,13 +1,13 @@
-def bubble_sorting(list_x: list[int]):
-    """Сортировка методом пузырька в порядке возрастания"""
-    for _ in range(len(A) + 1 // 2): 
-        for i in range(1, len(A)):
-            if list_x[i - 1] > list_x[i]:
-                list_x[i], list_x[i - 1] = list_x[i - 1], list_x[i]         
-    print(list_x)
+def bubble_sorting(list_x: list[int | float]) -> list[int | float]:
+    """Сортировка методом пузырька в порядке возрастания."""
+    for _ in range((len(list_x)+1) // 2):
+        for index in range(1, len(list_x)):
+            if list_x[index - 1] > list_x[index]:
+                list_x[index], list_x[index - 1] = list_x[index - 1], list_x[index]
+    return list_x
 
 
 
 
-A: list[int] = [5, 2, 8, 1, 0, 4, 5, 7, 2, 3, 9, 6, 0]
-bubble_sorting(A)
+list_num: list[int] = [5, 2, 8, 1, 0, 4, 5, 7, 2, 3, 9, 6, 0]
+print(bubble_sorting(list_num))
