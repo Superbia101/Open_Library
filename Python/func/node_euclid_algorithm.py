6fa883f429ff9dc24fc274e7e11de48afe20ea64
Superbia101 <cherkmark@yandex.ru>
2024-11-13 14:59:55 +0800

    Update node_euclid_algorithm.py

--- Python/func/node_euclid_algorithm.py
+++ Python/func/node_euclid_algorithm.py
@@ -1,3 +1,6 @@
+#from math import gcd
+
+
 def node_euclid_algorithm(num_1: int, num_2: int) -> int:
     """Поиск наибольшего общего делителя, через алгоритм Евклида.
 
@@ -9,14 +12,17 @@ def node_euclid_algorithm(num_1: int, num_2: int) -> int:
     :rtype: int
     :return: num_2
     """
+    
     num_1, num_2 = max(num_1, num_2), min(num_1, num_2)
-
-    while num_1 % num_2 != 0:
-        num_1, num_2 = num_2, num_1 % num_2
+    temp: int = num_1 % num_2
+    while temp != 0:
+        num_1, num_2 = num_2, temp
+        temp = num_1 % num_2
 
     return num_2
 
 
-print('НОД двух чисел:', node_euclid_algorithm(int(input('Введите 1-е число: ')),
-                                               int(input('Введите 2-е число: '))))
-#print('НОД двух чисел:', gcd(num_1, num_2))  # Проверка результата
+num1 = int(input('Введите 1-е число: '))
+num2 = int(input('Введите 2-е число: '))
+print('НОД двух чисел:', node_euclid_algorithm(num1, num2))
+#print('НОД двух чисел:', gcd(num1, num2)) # Проверка результата
